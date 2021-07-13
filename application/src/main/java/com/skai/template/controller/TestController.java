9fb0b32f3a042c9e3fd91d88d0a33e44dffd7116
Maxim Marusich <maxim.marusich@kenshoo.com>
2021-07-13 14:06:47 +0300

    Configure metrics reporting (#8)

--- application/src/main/java/com/skai/template/controller/TestController.java
+++ application/src/main/java/com/skai/template/controller/TestController.java
@@ -1,19 +1,30 @@
 package com.skai.template.controller;
 
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.MeterRegistry;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.codahale.metrics.MetricRegistry.name;
+
 @RestController
 @RequestMapping(value = "api/v1/test")
+@RequiredArgsConstructor
 public class TestController {
 
+    private final MeterRegistry meterRegistry;
+
+    @Counted
     @GetMapping
     public String get() {
-        return "You successfully sent a GET request";
+        return meterRegistry.timer(name(this.getClass(), "get")).record(() -> "You successfully sent a GET request");
     }
 
+    @Timed
     @PostMapping
     public String post() {
         return "You successfully sent a POST request";
