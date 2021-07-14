cc7d90281b55fe844659e16707f6fd5edc08b5bf
Maxim Marusich <maxim.marusich@kenshoo.com>
2021-07-14 15:48:56 +0300

    Configure logging, JSON format and Plain for development (#10)

--- application/src/main/java/com/skai/template/controller/TestController.java
+++ application/src/main/java/com/skai/template/controller/TestController.java
@@ -4,14 +4,19 @@ import io.micrometer.core.annotation.Counted;
 import io.micrometer.core.annotation.Timed;
 import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.logstash.logback.marker.Markers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 import static com.codahale.metrics.MetricRegistry.name;
 
 @RestController
+@Slf4j
 @RequestMapping(value = "api/v1/test")
 @RequiredArgsConstructor
 public class TestController {
@@ -23,6 +28,7 @@ public class TestController {
     @Counted
     @GetMapping
     public String get() {
+        log.info(Markers.append("userId", UUID.randomUUID().toString()), "Received request to test controller");
         return meterRegistry.timer(GET_REQUEST_METRIC_NAME).record(() -> "You successfully sent a GET request");
     }
 
--- application/src/main/java/com/skai/template/dataaccess/dao/impl/UserDaoImpl.java
+++ application/src/main/java/com/skai/template/dataaccess/dao/impl/UserDaoImpl.java
@@ -4,12 +4,14 @@ import com.skai.template.dataaccess.dao.UserDao;
 import com.skai.template.dataaccess.entities.User;
 import com.skai.template.dataaccess.table.UserTable;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
+@Slf4j
 @RequiredArgsConstructor
 public class UserDaoImpl implements UserDao {
 
@@ -17,6 +19,7 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public long create(User user) {
+        log.info("Creating user : {}", user);
         return dslContext.insertInto(UserTable.TABLE, UserTable.TABLE.name)
                 .values(user.getName())
                 .execute();
@@ -24,6 +27,7 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public Optional<User> findByName(String name) {
+        log.info("Searching user in DB by name : {}", name);
         User user = dslContext.select(UserTable.TABLE.id, UserTable.TABLE.name)
                 .from(UserTable.TABLE)
                 .where(UserTable.TABLE.name.equal(name))
