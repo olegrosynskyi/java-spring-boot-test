f1f0a584e2d58f58da4ece0100147cb467b0ad33
Maxim Marusich <maxim.marusich@kenshoo.com>
2021-07-20 17:09:08 +0300

    Fix environment for integration tests (#16)

--- application/src/test/java/com/skai/template/dataaccess/dao/UserDaoIntegrationTest.java
+++ application/src/test/java/com/skai/template/dataaccess/dao/UserDaoIntegrationTest.java
@@ -8,13 +8,11 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@ActiveProfiles("test")
 @SpringBootTest(classes = Application.class)
 public class UserDaoIntegrationTest {
 
