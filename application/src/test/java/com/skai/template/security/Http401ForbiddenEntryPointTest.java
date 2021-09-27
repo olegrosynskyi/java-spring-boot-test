1a0f90648595e0c48a0b299f7678961e13b2e6c3
Maxim Marusich <maxim.marusich@kenshoo.com>
2021-09-27 17:54:46 +0300

    Increase test coverage and restriction (#46)
    Co-authored-by: kgithub-liquik <github+liquik@kenshoo.com>

---
+++ application/src/test/java/com/skai/template/security/Http401ForbiddenEntryPointTest.java
@@ -0,0 +1,55 @@
+package com.skai.template.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class Http401ForbiddenEntryPointTest {
+
+    @InjectMocks
+    private Http401ForbiddenEntryPoint entryPoint;
+    @Mock
+    private ObjectMapper objectMapper;
+
+    @Captor
+    private ArgumentCaptor<Map<String, Object>> captor;
+
+    @Test
+    @SneakyThrows
+    public void responseContainsCorrectDetails() {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        given(response.getStatus()).willReturn(HttpServletResponse.SC_UNAUTHORIZED);
+        given(request.getServletPath()).willReturn("/api/v1/test");
+        given(response.getWriter()).willReturn(mock(PrintWriter.class));
+
+        entryPoint.commence(request, response, null);
+
+        verify(response).setContentType(MediaType.APPLICATION_JSON_VALUE);
+        verify(response).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        verify(objectMapper).writeValueAsString(captor.capture());
+        Map<String, Object> responseDetails = captor.getValue();
+        assertThat(responseDetails.get("status"), equalTo(HttpServletResponse.SC_UNAUTHORIZED));
+        assertThat(responseDetails.get("path"), equalTo("/api/v1/test"));
+        assertThat(responseDetails.get("message"), equalTo("Access Denied. Credentials are required to access this resource."));
+    }
+}
---
+++ application/src/test/java/com/skai/template/security/JWTAuthenticationFilterTest.java
@@ -0,0 +1,92 @@
+package com.skai.template.security;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class JWTAuthenticationFilterTest {
+
+    @InjectMocks
+    private JWTAuthenticationFilter filter;
+    @Mock
+    private AuthenticationManager authenticationManager;
+
+    private final HttpServletRequest request = mock(HttpServletRequest.class);
+    private final HttpServletResponse response = mock(HttpServletResponse.class);
+    private final FilterChain filterChain = mock(FilterChain.class);
+
+    @Captor
+    private ArgumentCaptor<JWTAuthenticationToken> captor;
+
+    @Test
+    @SneakyThrows
+    public void continueFilterChainInCaseNoTokenProvided() {
+        given(request.getHeader(HttpHeaders.AUTHORIZATION)).willReturn(null);
+
+        filter.doFilterInternal(request, response, filterChain);
+
+        verify(filterChain).doFilter(request, response);
+        verify(authenticationManager, never()).authenticate(any(Authentication.class));
+        assertThat(SecurityContextHolder.getContext().getAuthentication(), nullValue());
+    }
+
+    @Test
+    @SneakyThrows
+    public void continueFilterChainIfTokenPrefixNotSupported() {
+        given(request.getHeader(HttpHeaders.AUTHORIZATION)).willReturn("not supported token");
+
+        filter.doFilterInternal(request, response, filterChain);
+
+        verify(filterChain).doFilter(request, response);
+        verify(authenticationManager, never()).authenticate(any(Authentication.class));
+        assertThat(SecurityContextHolder.getContext().getAuthentication(), nullValue());
+    }
+
+    @Test
+    @SneakyThrows
+    public void stopFilterChainInCaseOfError() {
+        given(request.getHeader(HttpHeaders.AUTHORIZATION)).willReturn(JWTAuthenticationFilter.PREFIX + " test token");
+        given(authenticationManager.authenticate(any(JWTAuthenticationToken.class))).willThrow(new BadCredentialsException("Provided credentials are not valid"));
+
+        filter.doFilterInternal(request, response, filterChain);
+
+        verify(filterChain, never()).doFilter(request, response);
+        assertThat(SecurityContextHolder.getContext().getAuthentication(), nullValue());
+    }
+
+    @Test
+    @SneakyThrows
+    public void authenticateInCaseOfValidToken() {
+        Authentication authResult = mock(Authentication.class);
+        given(request.getHeader(HttpHeaders.AUTHORIZATION)).willReturn(JWTAuthenticationFilter.PREFIX + "token");
+        given(authenticationManager.authenticate(captor.capture())).willReturn(authResult);
+
+        filter.doFilterInternal(request, response, filterChain);
+
+        JWTAuthenticationToken jwtAuthenticationToken = captor.getValue();
+        assertThat(jwtAuthenticationToken.getCredentials(), equalTo("token"));
+        verify(filterChain).doFilter(request, response);
+        assertThat(SecurityContextHolder.getContext().getAuthentication(), equalTo(authResult));
+    }
+}
